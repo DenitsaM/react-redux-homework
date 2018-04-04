65ddad4f81f949946aa6b0c425cb2924cf11b3f5
denitsa minkova <denitsa.minkova@ideagen.com>
2018-04-04 16:10:14 +0300

    removed unused file

--- homeworks/homework1/src/index.js
+++ homeworks/homework1/src/index.js
@@ -2,7 +2,6 @@ import './main.scss';
 import "babel-polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FilteredList from './js/components/search';
 import RickAndMortyComponent from'./js/components/RickAndMortyComponent'
 
 const characters = [];
--- homeworks/homework1/src/js/components/RickAndMortyComponent.jsx
+++ homeworks/homework1/src/js/components/RickAndMortyComponent.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Search from './search';
 
 class RickAndMortyComponent extends React.Component {
   constructor({ initialState }) {
